84cdd0a10b0d241d4a025e84974f2e78ae8a631c
JJEllacopulos <55036974+JJEllacopulos@users.noreply.github.com>
2019-10-15 20:10:50 -0300

    Add files via upload

---
+++ Practica de ramdom.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstdlib>
+#include<clocale>
+#include<ctime>
+using namespace std;
+
+int main (void){
+
+    int dado;
+    int cont;
+
+    srand(time(0));
+
+    for(cont = 0 ; cont < 10 ; cont++){
+    dado = ((rand()) % 100) + 1;
+
+    cout<< dado<< endl;
+    }
+
+    return 0;
+}
